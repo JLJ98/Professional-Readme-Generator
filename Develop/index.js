ec672c57d2b046fa081af820dd91af1b6bfba77f
Jon-Luke <jonlukejenkins2@gmail.com>
2024-01-06 11:01:48 -0600

    Added functionality to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project:',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateReadmeContent();
+        writeToFile('README.md', readmeContent);
+    });
+}
+
+// Additional helper function to generate the README content
+function generateReadmeContent() {
+    // Use answers to generate and return the README content
+    // This function will depend on how you want to format your README based on the user's input
+}
 
 // Function call to initialize app
 init();
